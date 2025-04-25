eb09a1f686f86ff1af7f4186c767404c16b6f91b
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-04-25 20:17:55 +0200

    correct getEnvVar, server.js

--- src/server.js
+++ src/server.js
@@ -41,7 +41,7 @@ export const setupServer = () => {
 
     res.json({
       status: 200,
-      message: 'Successfully found contact with id ${contactId}!',
+      message: `Successfully found contact with id ${contactId}!`,
       data,
     });
   });
