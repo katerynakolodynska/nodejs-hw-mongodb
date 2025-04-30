1491f48866b866809cd12cc7de0074469f74b5b3
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-04-30 12:18:49 +0200

    valid import

--- src/routers/contact.js
+++ src/routers/contact.js
@@ -16,7 +16,7 @@ import {
   contactUpdateSchema,
 } from '../validation/contact.js';
 
-import { isValidId } from '.src/middlewares/isValidId.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const contactRouter = Router();
 
@@ -32,13 +32,6 @@ contactRouter.post(
   validateBody(contactAddSchema),
   ctrlWrapper(addContactController),
 );
-
-// contactRouter.put(
-//   '/:contactId',
-//   isValidId,
-//   validateBody(contactAddSchema),
-//   ctrlWrapper(upsertContactController),
-// );
 
 contactRouter.patch(
   '/:contactId',
