bbcdd8133b5e15c2877ee5e2723d907fff5165ca
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-05-03 11:29:38 +0200

    total correct

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -19,15 +19,15 @@ export const getContacts = async ({
     contactQuery.where('contactType').equals(filters.type);
   }
 
-  const data = await contactQuery
-    .skip(skip)
-    .limit(perPage)
-    .sort({ [sortBy]: sortOrder });
-
   const totalItems = await contactCollection
     .find()
     .merge(contactQuery)
     .countDocuments();
+
+  const data = await contactQuery
+    .skip(skip)
+    .limit(perPage)
+    .sort({ [sortBy]: sortOrder });
 
   const paginationData = calcPaginationData({ page, perPage, totalItems });
 
