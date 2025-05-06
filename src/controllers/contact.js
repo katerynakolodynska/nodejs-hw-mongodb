a870d660cdbe150934d126c5eca4869ed23436d3
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-05-06 19:02:10 +0200

    correct patch

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -118,7 +118,7 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact! ',
-    data: result,
+    data: result.data,
   });
 };
 
