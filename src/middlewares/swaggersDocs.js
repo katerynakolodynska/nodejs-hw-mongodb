e4f4256a3a46d9078ae01168582a51c673466dfb
Kateryna Kolodynska <kolodinkata@gmail.com>
2025-05-07 20:45:22 +0200

    correct hw7

--- src/middlewares/swaggersDocs.js
+++ src/middlewares/swaggersDocs.js
@@ -1,17 +1,13 @@
 import swaggerUI from 'swagger-ui-express';
-import readFileSync from 'node:fs';
+import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = () =>
-      json.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-  } catch (err) {
-    return (req, res) => {
-      res.status(500).json({
-        message: 'Can`t load swagger docs',
-      });
-    };
+  } catch {
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
   }
 };
